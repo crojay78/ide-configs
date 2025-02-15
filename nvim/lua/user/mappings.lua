64e9009641d8cefaa1fde9e71e6f0cf17c211649
crojay <crojay78@gmail.com>
2025-02-15 03:12:15 +0100

    changed: added gm for astronvim

--- nvim/lua/user/mappings.lua
+++ nvim/lua/user/mappings.lua
@@ -6,8 +6,12 @@ vim.keymap.set("n", "L", ":bprev<CR>", { noremap = true, silent = true })
 
 vim.keymap.set("n", "<leader>tt", ":terminal<CR>", { noremap = true, silent = true })
 vim.keymap.set("n", "<leader>t", "<cmd>TodoTelescope<CR>", { noremap = true, silent = true })
-vim.keymap.set("n", "<leader>mp", "<cmd>MarkdownPreview<CR>", { noremap = true, silent = true })
 
+vim.keymap.set("n", "gm", function()
+  require("telescope.builtin").lsp_document_symbols({
+    symbols = { "Function", "Method" }, -- Zeigt nur Methoden/Funktionen
+  })
+end, { noremap = true, silent = true })
 -- Verschiebe Zeilen nach unten im visuellen Modus
 vim.api.nvim_set_keymap("v", "J", ":m '>+1<CR>gv=gv", { noremap = true, silent = true })
 -- Verschiebe Zeilen nach oben im visuellen Modus
