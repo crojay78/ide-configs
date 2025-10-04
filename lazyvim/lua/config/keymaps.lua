52426cb5b8d50827a8bdef5c291e12d196872be8
github-oj <oj-software-projects@gmail.com>
2025-10-04 12:50:12 +0200

    changed: current ideavimrc added

---
+++ lazyvim/lua/config/keymaps.lua
@@ -0,0 +1,3 @@
+vim.keymap.set("n", "L", ":bnext<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "H", ":bprev<CR>", { noremap = true, silent = true })
+vim.keymap.set("i", "jj", "<ESC>", { noremap = true, silent = true })
